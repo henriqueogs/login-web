137ee92f6adaccc7769198ab6fe7be0ceeb444ce
Henrique Guimaraes <henrique.guimaraes@dommus.com.br>
2021-03-08 20:42:46 -0300

    Atualizando server

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,4 @@
-export const TOKEN_KEY = "@wiser-Token";
+export const TOKEN_KEY = "@login-Token";
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const login = token => {
