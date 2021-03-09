c795517a5705616957cd208de2046ef544a13a0a
Henrique Guimaraes <henrique.guimaraes@dommus.com.br>
2021-03-09 00:41:40 -0300

    Alterando Back

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "https://login-server.vercel.app/"
+  baseURL: "https://login-henriqueogs.herokuapp.com/"
 });
 
 api.interceptors.request.use(async config => {
